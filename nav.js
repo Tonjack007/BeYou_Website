d69a39ba76ddc989fa08513d22163ded19e65d56
Korawit Utai <korawitutai@gmail.com>
2023-04-03 23:28:49 +0200

    first commit

---
+++ nav.js
@@ -0,0 +1,10 @@
+
+const burgerNav = document.querySelector('.burgerNav')
+const navLinks = document.querySelector('.navLinks')
+
+burgerNav.addEventListener('click', () => {
+
+  navLinks.classList.toggle('active')
+})
+
+
